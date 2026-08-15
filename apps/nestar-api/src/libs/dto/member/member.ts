4b74938d0dcf8be063f16d6f6feb0bfba78c1e85
Ray <firdavsyangiev@gmail.com>
2026-08-15 09:55:25 +0900

    feat: develop login business logic

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -1,26 +1,26 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongoose';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
 
 @ObjectType()
 export class Member {
 	@Field(() => String)
-	_id!: ObjectId;
+	_id: ObjectId;
 
 	@Field(() => MemberType)
-	memberType!: MemberType;
+	memberType: MemberType;
 
 	@Field(() => MemberStatus)
-	memberStatus!: MemberStatus;
+	memberStatus: MemberStatus;
 
 	@Field(() => MemberAuthType)
-	memberAuthType!: MemberAuthType;
+	memberAuthType: MemberAuthType;
 
 	@Field(() => String)
-	memberPhone!: string;
+	memberPhone: string;
 
 	@Field(() => String)
-	memberNick!: string;
+	memberNick: string;
 
 	memberPassword?: string;
 
@@ -28,7 +28,7 @@ export class Member {
 	memberFullName?: string;
 
 	@Field(() => String)
-	memberImage!: string;
+	memberImage: string;
 
 	@Field(() => String, { nullable: true })
 	memberAddress?: string;
@@ -37,41 +37,41 @@ export class Member {
 	memberDesc?: string;
 
 	@Field(() => Int)
-	memberProperties!: number;
+	memberProperties: number;
 
 	@Field(() => Int)
-	memberArticles!: number;
+	memberArticles: number;
 
 	@Field(() => Int)
-	memberFollowers!: number;
+	memberFollowers: number;
 
 	@Field(() => Int)
-	memberFollowing!: number;
+	memberFollowing: number;
 
 	@Field(() => Int)
-	memberPoints!: number;
+	memberPoints: number;
 
 	@Field(() => Int)
-	memberLikes!: number;
+	memberLikes: number;
 
 	@Field(() => Int)
-	memberViews!: number;
+	memberViews: number;
 
 	@Field(() => Int)
-	memberComments!: number;
+	memberComments: number;
 
 	@Field(() => Int)
-	memberRank!: number;
+	memberRank: number;
 
 	@Field(() => Int)
-	memberWarnings!: number;
+	memberWarnings: number;
 
 	@Field(() => Int)
-	memberBlocks!: number;
+	memberBlocks: number;
 
 	@Field(() => Date, { nullable: true })
-	deletedAt!: Date;
+	deletedAt: Date;
 
 	@Field(() => Date, { nullable: true })
-	updatedAt!: Date;
+	updatedAt: Date;
 }
