e15ab1deb6dc043a669b9ea499b5a7ffc6738571
Ray <firdavsyangiev@gmail.com>
2026-09-01 18:50:39 +0900

    feat: develop removeCommentByAdmin GraphQL API

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,15 +1,14 @@
-import { BadGatewayException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
-import { ViewService } from '../view/view.service';
 import { BoardArticleService } from '../board-article/board-article.service';
+import { PropertyService } from '../property/property.service';
 import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
-import { CommentUpdate } from '../../libs/dto/comment/comment.update';
-import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
-import { PropertyService } from '../property/property.service';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { T } from '../../libs/types/common';
 import { lookupMember } from '../../libs/config';
 
@@ -32,7 +31,7 @@ export class CommentService {
 		} catch (err) {
 			//@ts-ignore
 			console.log('Error, Service.model:', err.message);
-			throw new BadGatewayException(Message.CREATE_FAILED);
+			throw new BadRequestException(Message.CREATE_FAILED);
 		}
 
 		switch (input.commentGroup) {
@@ -43,6 +42,7 @@ export class CommentService {
 					modifier: 1,
 				});
 				break;
+
 			case CommentGroup.ARTICLE:
 				await this.boardArticleService.boardArticleStatsEditor({
 					_id: input.commentRefId,
@@ -50,6 +50,7 @@ export class CommentService {
 					modifier: 1,
 				});
 				break;
+
 			case CommentGroup.MEMBER:
 				await this.memberService.memberStatsEditor({
 					_id: input.commentRefId,
@@ -58,12 +59,15 @@ export class CommentService {
 				});
 				break;
 		}
+
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+
 		return result;
 	}
 
 	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
 		const { _id } = input;
+
 		const result = await this.commentModel.findOneAndUpdate(
 			{
 				_id: _id,
@@ -75,7 +79,9 @@ export class CommentService {
 				new: true,
 			},
 		);
+
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
 		return result;
 	}
 
@@ -84,7 +90,7 @@ export class CommentService {
 		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
 		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-		const result = await this.commentModel.aggregate([
+		const result: Comments[] = await this.commentModel.aggregate([
 			{ $match: match },
 			{ $sort: sort },
 			{
@@ -92,7 +98,7 @@ export class CommentService {
 					list: [
 						{ $skip: (input.page - 1) * input.limit },
 						{ $limit: input.limit },
-						//meLiked
+						// meLiked
 						lookupMember,
 						{ $unwind: '$memberData' },
 					],
@@ -100,8 +106,16 @@ export class CommentService {
 				},
 			},
 		]);
+
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
+	}
+	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input);
+
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
 	}
 }
